0689ef2afcdd9a05b0afe6356bb7713db6a16594
Davi <werdavid1@gmail.com>
2023-08-31 11:48:22 -0300

    Primeiro Commit

---
+++ Aplicacaowebds/Aplicacaowebds/Models/Cliente.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+
+namespace Aplicacaowebds.Models
+{
+    public class Cliente
+    {
+
+        [Display(Name = "Nome do Cliente ")]
+        [Required(ErrorMessage = "Por Favor preencha seu Nome")]
+        private string Nome { get; set; }
+
+
+        [Display(Name = "Endereço")]
+        [Required(ErrorMessage = "Por Favor preencha seu Endereço")]
+        private string Endereco { get; set; }
+
+
+
+        [Display(Name = "Telefone")]
+        [Required(ErrorMessage = "Digite seu Telefone")]
+        [RegularExpression("^[0-9]{2}-([0-9]{8}|[0-9]{9})")]
+        private string Telefone { get; set; }
+
+
+
+        [Display(Name = "Email")]
+        [RegularExpression(@"\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*")]
+        private string Email { get; set; }
+
+
+        [Display(Name = "CPF")]
+        [Required(ErrorMessage = " Preencha com seu CPF")]
+        [StringLength(11)]
+        private int CPF { get; set; }
+
+
+        [Display(Name = "Data de Nascimento")]
+        [Required(ErrorMessage = "Preencha com sua Data de Nascimento")]
+        [DisplayFormat(DataFormatString = "{dd/mm/aaa}")]
+        private DateTime DataNascimento { get; set; }
+    }
+}
